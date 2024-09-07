31515498004d7d082e279d2f78a93ad47b1afd7b
Jayden Omollo <Jaydendejong83@gmail.com>
2024-09-07 13:40:54 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}",  // Adjust paths as needed
+   
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
